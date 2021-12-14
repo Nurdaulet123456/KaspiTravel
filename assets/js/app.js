a097863a442e4f10623fb7a090f2025599862c9b
Nurdaulet <88930614+frontendNurdaulet@users.noreply.github.com>
2021-12-14 22:00:47 +0600

    HelloKaspi

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+const input = document.getElementById('date');
+const input2 = document.getElementById('date2');
+const datepicker = new TheDatepicker.Datepicker(input);
+const datepicker2 = new TheDatepicker.Datepicker(input2);
+datepicker.render();
+datepicker2.render();
+
+
+
+// Loader
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.querySelector(".page").style.display = "block";
+}
+
+
+// Burger
